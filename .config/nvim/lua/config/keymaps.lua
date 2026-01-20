3bb2ed61461cdd87a2b8587bdeae7169e812a122
SuzuyaBish <sloan.jarrod@proton.me>
2026-01-20 13:06:44 +0200

    feat(cursor): Add cursor keymaps to nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,31 @@
 local map = vim.keymap.set
+local vscode = require("vscode")
 
--- Insert & Visual
-map({ "v", "i" }, "kj", "<Esc>", { desc = "Escape", silent = true })
+if vim.g.vscode then
+  map({ "n" }, "<leader>bo", function()
+    vscode.action("workbench.action.closeOtherEditors")
+  end)
 
--- Normal
-map({ "n" }, "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab", silent = true })
-map({ "n" }, "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Tab", silent = true })
+  map({ "n" }, "<leader>ff", function()
+    vscode.action("workbench.action.quickOpen")
+  end)
+
+  map({ "n" }, "<leader>ca", function()
+    vscode.action("editor.action.quickFix")
+  end)
+
+  map({ "n" }, "<leader>cr", function()
+    vscode.action("editor.action.rename")
+  end)
+
+  map({ "n" }, "<leader>bd", function()
+    vscode.action("workbench.action.closeActiveEditor")
+  end)
+else
+  -- Insert & Visual
+  map({ "v", "i" }, "kj", "<Esc>", { desc = "Escape", silent = true })
+
+  -- Normal
+  map({ "n" }, "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab", silent = true })
+  map({ "n" }, "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Tab", silent = true })
+end

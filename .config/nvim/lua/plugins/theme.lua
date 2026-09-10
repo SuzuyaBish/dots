51577a2302442b5ff6444cb3b527e304649177a8
SuzuyaBish <sloan.jarrod@proton.me>
2026-09-10 09:18:47 +0200

    ui: Update neovim theme to match ghostty

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,26 +1,19 @@
 return {
-  -- {
-  --   "webhooked/kanso.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("kanso").setup({
-  --       background = {
-  --         dark = "zen",
-  --         light = "pearl",
-  --       },
-  --     })
-  --   end,
-  -- },
   {
-    "dgox16/oldworld.nvim",
-    lazy = false,
-    priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          transparency = true,
+        },
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oldworld",
+      colorscheme = "rose-pine",
       lazy = true,
       priority = 1000,
     },
